d00efe4e15c437c2ae9a518e18a5e082d217507a
SeinekVoytov <voytov1704@gmail.com>
2023-09-27 17:41:29 +0300

    Add class JsonSerializationUtil class that is responsible for serializing objects to JSON.

---
+++ src/main/java/com/example/httplearningapi/util/JsonSerializationUtil.java
@@ -0,0 +1,23 @@
+package com.example.httplearningapi.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class JsonSerializationUtil {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public static <T> String serializeObjectToJsonString(T obj) throws JsonProcessingException {
+        return MAPPER.writeValueAsString(obj);
+    }
+
+    public static <T> void serializeObjectToJsonStream(T obj, OutputStream os) throws IOException {
+        String serializationResult = serializeObjectToJsonString(obj);
+        os.write(serializationResult.getBytes());
+    }
+
+
+}
