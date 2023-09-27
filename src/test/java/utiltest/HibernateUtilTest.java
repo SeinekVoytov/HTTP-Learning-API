079edf54f925784eb3a77dc50276171bbb9961f7
SeinekVoytov <voytov1704@gmail.com>
2023-09-27 19:29:11 +0300

    Add HibernateUtilTest class in which functionality of HibernateUtil class is tested.

---
+++ src/test/java/utiltest/HibernateUtilTest.java
@@ -0,0 +1,13 @@
+package utiltest;
+
+import com.example.httplearningapi.util.HibernateUtil;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class HibernateUtilTest {
+
+    @Test
+    public void classInitializationTest() {
+        assertNotNull(HibernateUtil.getSessionFactory(), "SessionFactory shouldn't be null");
+    }
+}
