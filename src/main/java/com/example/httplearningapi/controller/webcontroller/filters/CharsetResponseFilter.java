3dbc344dbdceff4b78e30776d40cabd22d0d5b6c
SeinekVoytov <voytov1704@gmail.com>
2023-10-04 16:55:48 +0300

    Create CharsetRequestFilter and CharsetResponseFilter classes that are responsible for setting UTF-8 charset for both request and response.

---
+++ src/main/java/com/example/httplearningapi/controller/webcontroller/filters/CharsetResponseFilter.java
@@ -0,0 +1,24 @@
+package com.example.httplearningapi.controller.webcontroller.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "Charset Response Filter", urlPatterns = {"/*"})
+public class CharsetResponseFilter implements Filter {
+
+    private static final String CHARSET_UTF_8 = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setCharacterEncoding(CHARSET_UTF_8);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {}
+}
