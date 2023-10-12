51302e2112186126f5a9670b385b283b813c489a
SeinekVoytov <voytov1704@gmail.com>
2023-10-12 17:49:02 +0300

    Implement PrescriptionService.handleGet method

--- src/main/java/com/example/httplearningapi/model/service/PrescriptionService.java
+++ src/main/java/com/example/httplearningapi/model/service/PrescriptionService.java
@@ -1,15 +1,50 @@
 package com.example.httplearningapi.model.service;
 
+import com.example.httplearningapi.model.dao.Dao;
+import com.example.httplearningapi.model.dao.PrescriptionDao;
+import com.example.httplearningapi.model.entities.user.Prescription;
+import com.example.httplearningapi.model.entities.user.User;
+import com.example.httplearningapi.util.JsonSerializationUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PrescriptionService implements Service {
-    @Override
-    public void handleGet(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    private final Dao<Prescription> prescriptionDao = new PrescriptionDao();
+
+    @Override
+    public void handleGet(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        if (pathInfo == null || pathInfo.equals("/")) {
+
+            User userAttribute = (User) req.getAttribute("user");
+            List<Prescription> prescriptions = (userAttribute == null) ?
+                    prescriptionDao.getAll() : userAttribute.getPrescriptions();
+
+            String queryString = req.getQueryString();
+            if (queryString != null) {
+                prescriptions = this.filterByQueryParams(req, prescriptions);
+            }
+
+            JsonSerializationUtil.serializeObjectToJsonStream(prescriptions, resp.getWriter());
+            return;
+        }
+
+        int prescriptionId = extractIdFromURI(pathInfo);
+        Prescription prescription = prescriptionDao.getById(prescriptionId).orElseThrow();
+
+        if (pathInfo.matches("^/[^/]+/?$")) {
+            JsonSerializationUtil.serializeObjectToJsonStream(prescription, resp.getWriter());
+            return;
+        }
+
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
 
     @Override
@@ -26,4 +61,23 @@ public class PrescriptionService implements Service {
     public void handleDelete(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
+
+    private Predicate<Prescription> createPredicateForFilteringByQueryParams(HttpServletRequest req) {
+        String id = req.getParameter("id");
+        String medicationName = req.getParameter("medication");
+        return prescription ->
+                (id == null || id.equals(String.valueOf(prescription.getId()))) &&
+                (medicationName == null || medicationName.equals(prescription.getMedicationName()));
+    }
+
+    private List<Prescription> filterByQueryParams(HttpServletRequest req, List<Prescription> targetList) {
+        return targetList.stream()
+                .filter(this.createPredicateForFilteringByQueryParams(req))
+                .collect(Collectors.toList());
+    }
+
+    private int extractIdFromURI(String pathInfo) {
+        return Integer.parseInt(pathInfo.substring(1).split("/")[0]);
+    }
+
 }
