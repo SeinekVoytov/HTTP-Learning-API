0342ed41bedafdfdf7d2211e3da66ae602df724d
SeinekVoytov <voytov1704@gmail.com>
2023-10-12 16:57:03 +0300

    Create PrescriptionService class to process Get, Post, Put and Delete requests.

---
+++ src/main/java/com/example/httplearningapi/model/service/PrescriptionService.java
@@ -0,0 +1,29 @@
+package com.example.httplearningapi.model.service;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class PrescriptionService implements Service {
+    @Override
+    public void handleGet(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void handlePost(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void handlePut(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void handleDelete(String pathInfo, HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
