ee9dcad44932eb2580182e55ef73609157807f4e
SeinekVoytov <voytov1704@gmail.com>
2023-10-30 19:55:46 +0300

    Provide PostDao class with some testcases

---
+++ src/test/java/daotest/PostDaoTest.java
@@ -0,0 +1,40 @@
+package daotest;
+
+import com.example.httplearningapi.model.dao.Dao;
+import com.example.httplearningapi.model.dao.PostDao;
+import com.example.httplearningapi.model.entities.user.Post;
+import extensions.HibernateUtilSetupExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(HibernateUtilSetupExtension.class)
+public class PostDaoTest {
+
+    private static final int NUMBER_OF_PRESCRIPTIONS_IN_DATABASE = 100;
+    private static final Dao<Post> PRESCRIPTION_DAO = new PostDao();
+
+    @Test
+    public void retrievedUserFromDBIsNotNullTest() {
+        Optional<Post> testPrescriptionOptional = PRESCRIPTION_DAO.getById(1);
+        if (testPrescriptionOptional.isEmpty()) {
+            fail("User is not retrieved from Database");
+        }
+
+        Post testPrescription = testPrescriptionOptional.get();
+        assertTrue(testPrescription.getAuthor() != null &&
+                testPrescription.getBody() != null &&
+                testPrescription.getTitle() != null, "Post fields are null");
+    }
+
+    @Test
+    public void numberOfUsersRetrievedFromDBIsEqualToExpectedNumberTest() {
+        List<Post> prescriptions = PRESCRIPTION_DAO.getAll();
+        assertEquals(NUMBER_OF_PRESCRIPTIONS_IN_DATABASE, prescriptions.size(),
+                "Actual number of Users in Database is not equal to Expected number");
+    }
+}
