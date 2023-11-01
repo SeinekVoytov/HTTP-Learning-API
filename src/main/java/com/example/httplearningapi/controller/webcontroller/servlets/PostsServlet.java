eee313564cd50bfb4d583352d915ec78c20bbf24
SeinekVoytov <voytov1704@gmail.com>
2023-11-01 17:55:07 +0300

    Create to PostsServlet class to handle all request with patterns "/posts", "/posts/*"

---
+++ src/main/java/com/example/httplearningapi/controller/webcontroller/servlets/PostsServlet.java
@@ -0,0 +1,22 @@
+package com.example.httplearningapi.controller.webcontroller.servlets;
+
+import com.example.httplearningapi.model.entities.user.Post;
+import com.example.httplearningapi.model.service.AbstractService;
+import com.example.httplearningapi.model.service.PostService;
+import com.example.httplearningapi.util.ExceptionHandleUtil;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/posts", "/posts/*"})
+public class PostsServlet extends AbstractServlet {
+
+    @Override
+    void handleRequest(HandleProcessor<AbstractService<?>> handleProcessor, HttpServletResponse resp) {
+        try {
+            AbstractService<Post> service = new PostService();
+            handleProcessor.process(service);
+        } catch (Exception e) {
+            ExceptionHandleUtil.processException(e, resp);
+        }
+    }
+}
