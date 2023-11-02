71244fe7fd6a460104d38cb55b58370610bb990c
SeinekVoytov <voytov1704@gmail.com>
2023-11-02 21:43:08 +0300

    Create CommentServlet to handle all requests with URI pattern: "/comments"

---
+++ src/main/java/com/example/httplearningapi/controller/webcontroller/servlets/CommentServlet.java
@@ -0,0 +1,23 @@
+package com.example.httplearningapi.controller.webcontroller.servlets;
+
+import com.example.httplearningapi.model.entities.comment.Comment;
+import com.example.httplearningapi.model.service.AbstractService;
+import com.example.httplearningapi.model.service.CommentService;
+import com.example.httplearningapi.util.ExceptionHandleUtil;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/comments"})
+public class CommentServlet extends AbstractServlet {
+    @Override
+    void handleRequest(HandleProcessor<AbstractService<?>> handleProcessor, HttpServletResponse resp) throws IOException {
+        try {
+            AbstractService<Comment> service = new CommentService();
+            handleProcessor.process(service);
+        } catch (Exception e) {
+            ExceptionHandleUtil.processException(e, resp);
+        }
+    }
+}
